f80a897f7558323043ebfe7951fc518085e2a1b7
mosinmiloluwa owoso <mosinmiloluwa.o@gmail.com>
2019-10-04 13:43:33 -0500

    remove unnecessary variables

--- server/routes/v1/casts.js
+++ server/routes/v1/casts.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import getMovieCasts from '<controllers>/movieCasts';
 import validateCastParams from '<middlewares>/validations/casts';
-import { validateParams } from '<middlewares>/validations/comments';
 
 const casts = express.Router();
 
