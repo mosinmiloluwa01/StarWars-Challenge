6984f60a1c8fbfd9f35f391872d61822b81ea27e
mosinmiloluwa owoso <mosinmiloluwa.o@gmail.com>
2019-10-08 18:20:44 -0500

    chore(implement-feedback): implement feedback
    - add movie id to response obect
    - correct response code for displayMessage function
    - modify query for sorting data
    - modify routes
    
    [Finishes #1690226118]

--- server/middlewares/validations/casts.js
+++ server/middlewares/validations/casts.js
@@ -2,16 +2,13 @@ import { displayMessage } from '<helpers>/utils';
 
 const validateCastParams = (req, res, next) => {
   const { sortParams } = req.query;
-  const { gender, id } = req.params;
+  const { id } = req.params;
   const errors = [];
 
   if (/\D/g.test(id) || +id < 1) {
     errors.push(' \'id\' must be a postive integer that is greater than 0');
   }
-  if (!['male', 'female'].includes(gender)) {
-    errors.push('gender should either be \'male\', \'female\'');
-  }
-  if (!['name', 'gender', 'height'].includes(sortParams)) {
+  if (sortParams && !['name', 'gender', 'height'].includes(sortParams)) {
     errors.push('sortParams should either be \'name\', \'gender\', \'height\'');
   }
   if (errors.length === 0) {
--- server/routes/v1/movies.js
+++ server/routes/v1/movies.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import getMovieList from '<controllers>/movies';
+import getMovieCasts from '<controllers>/movieCasts';
+import { createComment, getComments } from '<controllers>/comments';
+import validateCastParams from '<middlewares>/validations/casts';
+import { validateCommentInput, validateParams } from '<middlewares>/validations/comments';
 
 const movies = express.Router();
 
 movies.get('/', getMovieList);
+movies.get('/:id/casts', validateCastParams, getMovieCasts);
+movies.get('/:id/comments', validateParams, getComments);
+movies.post('/:id/comments', validateParams, validateCommentInput, createComment);
 
 
 export default movies;
